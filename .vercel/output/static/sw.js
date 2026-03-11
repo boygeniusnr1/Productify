c788ba19a4a698fecc78393bb498501dd0b2e8d7
boygeniusnr1 <boygeniusnr1@gmail.com>
2026-03-11 15:24:53 +1300

    Remove .vercel folder

--- .vercel/output/static/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-});
-
-self.addEventListener("fetch", (e) => {
-  // This allows the app to be installable
-  e.respondWith(fetch(e.request));
-});
